cb759b1ca6b79daa6d4499bed09147b15e6294b3
Eric Petersen <eric@huskers.unl.edu>
2014-04-27 18:43:13 -0500

    Adding Transaction model

--- transaction/models.py
+++ transaction/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Transaction(models.Model):
+    class Meta:
+        managed = True
+
+    guid = models.CharField(primary_key=True, max_length=32)
+    num = models.CharField(max_length=2048)
+    enter_date = models.CharField(max_length=14)  # TODO: format?
+    post_date = models.CharField(max_length=14)  # TODO: format?
+    description = models.CharField(max_length=2048)
+    currency_guid = models.CharField(max_length=32)  # ForeignKey...
